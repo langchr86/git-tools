c4184665097dea77b5d27fa79fe7d34c9d88adbc
Christian Lang <christian.lang@scs.ch>
2021-10-29 14:00:20 +0200

    ! fix problems with paths that contain whitespace for git-prompt

--- files/git-prompt.bash
+++ files/git-prompt.bash
@@ -3,9 +3,9 @@
 # GIT functions for better prompt
 
 function git-find-branch {
-  if [[ $head == ref:\ refs/heads/* ]]; then
+  if [[ "${head}" == ref:\ refs/heads/* ]]; then
     git_branch=" ${head#*/*/}"
-  elif [[ $head != '' ]]; then
+  elif [[ "${head}" != '' ]]; then
     git_branch=" ${head:0:6}"
   else
     git_branch=' (unknown)'
@@ -13,30 +13,30 @@ function git-find-branch {
 }
 
 function git-find-head-file {
-  local dir=$(pwd)
+  local dir="$(pwd)"
   local head=''
   local submod=''
-  until [ "$dir" -ef / ]; do
-  if [ -f "$dir/.git" ]; then
-    submod=`basename $dir`
+  until [ "${dir}" -ef / ]; do
+  if [ -f "${dir}/.git" ]; then
+    submod="$(basename "${dir}")"
     git_at="@"
     git_repo="${submod}"
-    until [ "$dir" -ef / ]; do
-      if [ -f "$dir/.git/modules/$submod/HEAD" ]; then
-        head=$(< "$dir/.git/modules/$submod/HEAD")
+    until [ "${dir}" -ef / ]; do
+      if [ -f "${dir}/.git/modules/${submod}/HEAD" ]; then
+        head=$(< "${dir}/.git/modules/${submod}/HEAD")
         git-find-branch
         return
       fi
-      dir=`dirname $dir`
+      dir="$(dirname ${dir})"
     done
-  elif [ -f "$dir/.git/HEAD" ]; then
+  elif [ -f "${dir}/.git/HEAD" ]; then
     git_at="@"
-    git_repo="`basename $dir`"
-    head=$(< "$dir/.git/HEAD")
+    git_repo="$(basename "${dir}")"
+    head=$(< "${dir}/.git/HEAD")
     git-find-branch
     return
   fi
-    dir=`dirname $dir`
+    dir="$(dirname "${dir}")"
   done
   git_branch=''
   git_at=''
@@ -44,4 +44,4 @@ function git-find-head-file {
 }
 
 # definition of bash prompt
-export GIT_PROMPT="\[\e[0m\][\[\e[0;32m\]\u\[\e[0m\]@\[\e[0;33m\]\h\[\e[0;35m\]\$git_branch \[\e[0m\]\w]$\[\e[0m\] "
+export GIT_PROMPT="\[\e[0m\][\[\e[0;32m\]\u\[\e[0m\]@\[\e[0;33m\]\h\[\e[0;35m\]\${git_branch} \[\e[0m\]\w]$\[\e[0m\] "
