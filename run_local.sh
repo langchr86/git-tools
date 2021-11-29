5695bcc5b04a224ca45dcbc54ff53f445e3a4780
Christian Lang <lang.chr86@gmail.com>
2021-11-29 23:03:32 +0100

    * use python3 in run_local.sh

--- run_local.sh
+++ run_local.sh
@@ -7,5 +7,6 @@ ansible-playbook \
   --connection=local \
   --inventory 127.0.0.1, \
   --limit 127.0.0.1 \
+  --extra-vars 'ansible_python_interpreter=/usr/bin/python3' \
   --ask-become-pass \
   testing/playbook.yml
